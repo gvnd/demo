07c52a1a6ace977737343e3008f46f776452f897
gvnd <govinda.polaki@gmail.com>
2019-07-05 16:55:45 +0530

    Added Ip address.

--- src/main/java/com/govinda/polaki/demo/DemoApplication.java
+++ src/main/java/com/govinda/polaki/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String ip = InetAddress.getLoopbackAddress().getHostAddress();
+		System.out.println(ip+" -----------------");
 	}
 
 }
